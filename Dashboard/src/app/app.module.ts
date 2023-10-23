bff87f9313cbce3107a1062748c0c682d95ec33d
Sejal Jain <sejalj519@gmail.com>
2023-10-23 22:53:55 +0530

    on Behalf Sejal

--- Dashboard/src/app/app.module.ts
+++ Dashboard/src/app/app.module.ts
@@ -4,17 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LeftNavigationComponent } from './left-navigation/left-navigation.component';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TopSectionComponent } from './top-section/top-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LeftNavigationComponent
+    LeftNavigationComponent,
+    TopSectionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [], // Remove the MatIconRegistry provider
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Dashboard/src/app/left-navigation/left-navigation.component.ts
+++ Dashboard/src/app/left-navigation/left-navigation.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { TopSectionComponent } from '../top-section/top-section.component';
+
 
 @Component({
   selector: 'app-left-navigation',
   templateUrl: './left-navigation.component.html',
-  styleUrls: ['./left-navigation.component.scss']
+  styleUrls: ['./left-navigation.component.scss'],
 })
 export class LeftNavigationComponent {
+
+  constructor(private iconRegistry: MatIconRegistry, private sanitizer: DomSanitizer) {
+  
+    
+   
+  }
+
 
 }
